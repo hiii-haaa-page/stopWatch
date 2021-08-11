d9143e35956d4265b40152ec9dc0d196300eaa5f
hiii-haaa-page <hashi.hiro.0531@gmail.com>
2021-08-11 11:32:09 +0900

    comoplete

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,63 @@
   const reset = document.getElementById('reset');
 
   let startTime;
+  let timeoutId;
+  let elapsedTime = 0;
+
   const countUp = () => {
-    console.log(Date.now() - startTime);
-    setTimeout(() => {
+    const d = new Date(Date.now() - startTime + elapsedTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+
+    timer.textContent = `${m}:${s}.${ms}`;
+    timeoutId = setTimeout(() => {
       countUp();
     }, 10);
   };
 
+  const setButtonStateInitial = () => {
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.add('inactive');
+  };
+  const setButtonStateRunning = () => {
+    start.classList.add('inactive');
+    stop.classList.remove('inactive');
+    reset.classList.add('inactive');
+  };
+  const setButtonStateStopped = () => {
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.remove('inactive');
+  };
+
+  setButtonStateInitial();
+
   start.addEventListener('click', () => {
+    if (start.classList.contains('inactive') === true) {
+      return;
+    }
     startTime = Date.now();
     countUp();
+    setButtonStateRunning();
+  });
+
+  stop.addEventListener('click', () => {
+    if (stop.classList.contains('inactive') === true) {
+      return;
+    }
+    clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime;
+    setButtonStateStopped();
+  });
+
+  reset.addEventListener('click', () => {
+    if (reset.classList.contains('inactive') === true) {
+      return;
+    }
+    timer.textContent = '00:00.000';
+    elapsedTime = 0;
+    setButtonStateInitial();
   });
 }
